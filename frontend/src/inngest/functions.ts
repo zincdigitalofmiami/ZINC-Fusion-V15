760af6fb84caab137b4493aee1575f53dc7af7c7
Kirk Musick, MS, MBA <zincmiami@gmail.com>
2026-01-08 14:28:45 -0600

    feat: add Inngest fusion-jobs

---
+++ frontend/src/inngest/functions.ts
@@ -0,0 +1,6 @@
+export { zlPrice } from "./zl-price";
+
+// Future jobs:
+// export { yahooEod } from "./yahoo-eod";
+// export { fredDaily } from "./fred-daily";
+// export { cftcWeekly } from "./cftc-weekly";
