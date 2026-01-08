d4489950f109a34ff9a0d8281808f17c0a0027cb
Kirk Musick, MS, MBA <zincmiami@gmail.com>
2026-01-08 15:27:19 -0600

    feat: activate Inngest zlPrice job

--- frontend/src/app/api/inngest/route.ts
+++ frontend/src/app/api/inngest/route.ts
@@ -1,12 +1,8 @@
 import { serve } from "inngest/next";
 import { inngest } from "@/inngest/client";
-// import { zlPrice } from "@/inngest/functions";
+import { zlPrice } from "@/inngest/functions";
 
-// Inngest integration ready but deactivated
-// Uncomment zlPrice when ready to enable 15-min price updates
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [
-    // zlPrice,  // Deactivated - uncomment to enable
-  ],
+  functions: [zlPrice],
 });
