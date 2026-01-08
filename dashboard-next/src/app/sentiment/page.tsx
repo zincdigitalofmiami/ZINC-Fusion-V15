bddf5588dfe2fa7dd8201fc7cbada473d8f615b5
Kirk Musick, MS, MBA <zincmiami@gmail.com>
2026-01-08 06:58:23 -0600

    chore: remove legacy Vue dashboard and transitional dashboard-next directories

--- dashboard-next/src/app/sentiment/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function SentimentPage() {
-  return (
-    <div className="page-content">
-      <h1 className="page-title">Sentiment</h1>
-      {/* TODO: Sentiment cards, source breakdown, headlines */}
-    </div>
-  )
-}
--- dashboard-next/src/app/strategy/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function StrategyPage() {
-  return (
-    <div className="page-content">
-      <h1 className="page-title">Strategy</h1>
-      {/* TODO: Current strategy header, reasoning chain, risk assessment */}
-    </div>
-  )
-}
